63f5d4c4528d7e1dd4a3d96da828dc178dda32dc
Chalk <chalk@chalk.pe>
2018-05-01 15:16:27 +0900

    show meal info

--- main.js
+++ main.js
@@ -1,23 +1,48 @@
+const ms = require('ms')
+const dimibob = require('dimibob')
+const package = require('./package.json')
+
 const path = require('path')
-const { app, Tray, Menu, BrowserWindow } = require('electron')
+const { app, shell, Tray, Menu, BrowserWindow } = require('electron')
 
 let tray = null
-let window = null
-let contextMenu = null
+let menu = null
 
 app.on('ready', function () {
-  window = new BrowserWindow({ show: false })
   tray = new Tray(path.join(__dirname, 'images', 'dimigo.png'))
+  menu = [
+    { id: 'date', enabled: false },
+    { id: 'breakfast' },
+    { id: 'lunch' },
+    { id: 'dinner' },
+    { id: 'snack' },
 
-  contextMenu = Menu.buildFromTemplate([
-    {
-      label: '급식'
-    },
+    { type: 'separator' },
+    { role: 'about', click: () => shell.openExternal(package.repository) },
+    { role: 'quit', click: () => app.quit() }
+  ]
 
-    {
-      label: '종료'
-    }
-  ])
-
-  tray.setContextMenu(contextMenu)
+  updateTray()
 })
+
+const menuItem = id => menu.find(item => item.id === id)
+const updateTray = () => tray.setContextMenu(Menu.buildFromTemplate(menu))
+
+async function updateMeal () {
+  try {
+    const bob = await dimibob()
+    const dt = bob.date.split('-')
+    menuItem('date').label = `${dt[0]}년 ${dt[1]}월 ${dt[2]}일의 급식`
+    menuItem('breakfast').label = `조식: ${bob.breakfast}`
+    menuItem('lunch').label = `중식: ${bob.lunch}`
+    menuItem('dinner').label = `석식: ${bob.dinner}`
+    menuItem('snack').label = `간식: ${bob.snack}`
+  } catch (err) {
+    console.error(err)
+  }
+
+  updateTray()
+}
+
+updateMeal()
+setInterval(updateMeal, ms('2h'))
